d1b021db1c6847a1a5decbb6d34d95c338d55fd8
subhashi <hydrogirls@gmail.com>
2020-03-21 21:19:23 +1100

    Updated the page title

--- homepage/views.py
+++ homepage/views.py
@@ -3,8 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-
-# from django.http import HttpResponse
-#
-# def home(request):
-#     return HttpResponse('home')
--- market/urls.py
+++ market/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.conf.urls import url
 from django.urls import path, include
-from .import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # url(r'^$', views.home),
     path('', include('homepage.urls')),
     path('fruits/', include('fruits.urls')),
     path('vegetables/', include('vegetables.urls'))
